c8ec5af3113c6c1515aa92fc2ea3b151cdc201d8
Axel HOMERY <axelhomery@hotmail.fr>
2022-03-25 14:21:17 +0100

    init project structure

---
+++ src/server.cpp
@@ -0,0 +1,8 @@
+#include "server.hpp"
+
+#include <iostream>
+#include <string>
+
+int main() {
+    return 0;
+}
---
+++ src/server.hpp
@@ -0,0 +1,15 @@
+#ifndef SERVER_HPP // Guard
+#define SERVER_HPP
+
+#include <string>
+
+class Server
+{
+public:
+    std::string name; // Un serveur est identifié par son nom
+
+    Server(); // Constructeur
+
+};
+
+#endif
